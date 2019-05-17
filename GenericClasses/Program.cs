864ffd1575f79f359cf559ace6fdfc27c0eedc92
Stephan Lunghi <slunghi43@gmail.com>
2019-05-17 12:24:45 -0400

    Extracted methods for a cleaner look. Added Console.Clear so it looks better when I run.

--- GenericClasses/Program.cs
+++ GenericClasses/Program.cs
@@ -11,53 +11,54 @@ namespace GenericClasses
     {
         static void Main(string[] args)
         {
-         List<int> list = new List<int>() {1, 2, 3, 4, 5};
-         Console.WriteLine("List");
+            ListDisplay();
+            Console.Clear();
+            LinkedListDisplay();
+            Console.Clear();
+            QueueDisplay();
+            Console.Clear();
+            StackDisplay();
+            Console.Clear();
+            DictionaryDisplay();
+            Console.Clear();
+            SortedListDisplay();
+            
 
-         foreach (var num in list)
-         {
-             
-             Console.WriteLine(num);
-         }
-         Console.ReadLine();
+        }
 
+        private static void SortedListDisplay()
+        {
+            Console.WriteLine("SortedList, Round two let the race begin!");
 
-         Console.WriteLine("LinkedList");
-         LinkedList<string> linkList = new LinkedList<string>();
+            SortedList<string, int> places2 = new SortedList<string, int>() { { "Peach", 1 }, { "Koopa", 2 }, { "Bowser", 3 }, { "Luigi", 4 }, { "Mario", 5 } };
 
-         foreach (var link in new string[] {"one", "two", "three", "four", "five"})
-         {
-             linkList.AddLast(link);
-         }
+            foreach (var place in places2)
+            {
+                Console.WriteLine(place);
+            }
 
-         foreach (var link in linkList)
-         {
-             Console.WriteLine(link);
-         }
-         Console.ReadLine();
+            Console.ReadLine();
+        }
 
+        private static void DictionaryDisplay()
+        {
+            Console.WriteLine("Dictionary, Let the Race begin!");
+            Dictionary<string, int> places = new Dictionary<string, int>() { { "Mario", 1 }, { "Luigi", 2 }, { "Peach", 3 }, { "Bowser", 4 }, { "Koopa", 5 } };
 
-            Console.WriteLine("Queue");
-         Queue<string> mario = new Queue<string>();
+            foreach (var place in places)
+            {
+                Console.WriteLine(place);
+            }
 
-         foreach (var mari in new string[]{"Mario", "Luigi", "Peach", "Bowser", "Koopa"})
-         {
-           mario.Enqueue(mari);
-           Console.WriteLine($"{mari} has joined the queue!");
-         }
-            Console.WriteLine("The queue contains:");
-         foreach (var mari in mario)
-         {
-             Console.WriteLine(mari);
-         }
+            Console.ReadLine();
+        }
 
-         Console.ReadLine();
-
-
+        private static void StackDisplay()
+        {
             Console.WriteLine("Stack");
             Stack<int> builds = new Stack<int>();
 
-            foreach (var build in new int[]{1, 2, 3, 4, 5})
+            foreach (var build in new int[] { 1, 2, 3, 4, 5 })
             {
                 builds.Push(build);
                 Console.WriteLine($"{build} has been pushed on the stack!");
@@ -70,27 +71,55 @@ namespace GenericClasses
             }
 
             Console.ReadLine();
+        }
 
-            Console.WriteLine("Dictionary, Let the Race begin!");
-            Dictionary<string, int> places = new Dictionary<string, int>(){{"Mario", 1}, {"Luigi", 2}, {"Peach", 3}, {"Bowser", 4}, {"Koopa", 5}};
+        private static void QueueDisplay()
+        {
+            Console.WriteLine("Queue");
+            Queue<string> mario = new Queue<string>();
 
-            foreach (var place in places)
+            foreach (var mari in new string[] { "Mario", "Luigi", "Peach", "Bowser", "Koopa" })
             {
-                Console.WriteLine(place);
+                mario.Enqueue(mari);
+                Console.WriteLine($"{mari} has joined the queue!");
+            }
+            Console.WriteLine("The queue contains:");
+            foreach (var mari in mario)
+            {
+                Console.WriteLine(mari);
             }
 
             Console.ReadLine();
+        }
 
-            Console.WriteLine("SortedList, Round two let the race begin!");
+        private static void LinkedListDisplay()
+        {
+            Console.WriteLine("LinkedList");
+            LinkedList<string> linkList = new LinkedList<string>();
 
-            SortedList<string, int> places2 = new SortedList<string, int>(){{"Peach", 1}, {"Koopa", 2}, {"Bowser", 3},{"Luigi", 4},{"Mario", 5}};
-
-            foreach (var place in places2)
+            foreach (var link in new string[] { "one", "two", "three", "four", "five" })
             {
-                Console.WriteLine(place);
+                linkList.AddLast(link);
             }
 
+            foreach (var link in linkList)
+            {
+                Console.WriteLine(link);
+            }
+            Console.ReadLine();
+        }
 
+        private static void ListDisplay()
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+            Console.WriteLine("List");
+
+            foreach (var num in list)
+            {
+
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
         }
     }
 }
